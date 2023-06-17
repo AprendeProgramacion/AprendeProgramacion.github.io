f6cf3e4e496746145727939a66df28e98badd7d8
PabloU <puribezuluaga28@gmail.com>
2023-06-17 18:29:10 -0500

    [OTHER: Design] (git): Sticky aside for topics

--- src/app/components/aside-temas/aside-temas.component.ts
+++ src/app/components/aside-temas/aside-temas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { TemasService } from 'src/app/services/temas.service';
 
 @Component({
@@ -10,9 +10,11 @@ export class AsideTemasComponent {
   visibility: boolean;
   view: string;
   links: any = [];
+  asidePos: string;
   constructor(private temas: TemasService) {
     this.visibility = false;
     this.view = 'hide';
+    this.asidePos="static";
   }
   ngOnInit(){
     this.temas.tema.subscribe((res)=>{
@@ -28,6 +30,46 @@ export class AsideTemasComponent {
           break;
       }
     });
+  }
+  ngAfterViewInit(){
+    this.setCurrent();
+  }
+
+  @HostListener('window:scroll')
+  onScroll(){
+    this.scrollAction()
+  }
+  scrollAction(){
+    if(window.innerWidth>=1026){
+      if(window.scrollY >=300){
+        this.asidePos="fixed";
+      }
+      else{
+        this.asidePos = "static";
+      }
+    }
+  }
+  setCurrent(){
+    setTimeout(()=>{
+      const ul = document.getElementById('list');
+      let list = ul?.getElementsByTagName('li');
+      if (list){
+        let lista = Array.from(list)
+        for(let i of lista){
+          let route = i.getElementsByTagName('a')[0]?.getAttribute("href");
+          if(route && route == window.location.pathname){
+            i.getElementsByTagName('a')[0].style.backgroundColor = "#ccc";
+            i.getElementsByTagName('a')[0].getElementsByTagName('span')[0].style.color = "#036d19";
+            i.getElementsByTagName('a')[0].getElementsByTagName('span')[0].style.fontWeight = "bold";
+          }
+          else{
+            i.getElementsByTagName('a')[0].style.backgroundColor = "#fff";
+            i.getElementsByTagName('a')[0].getElementsByTagName('span')[0].style.color = "black";
+            i.getElementsByTagName('a')[0].getElementsByTagName('span')[0].style.fontWeight = "normal";
+          }
+        }
+      }
+    },1);
   }
   hide(){
     this.visibility = false;
